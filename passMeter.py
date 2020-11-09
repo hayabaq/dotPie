1ad701ce634d4c6ee6ae915e81bb58b67796f2a7
hayabaq <33833299+hayabaq@users.noreply.github.com>
2020-11-09 12:58:05 -0500

    check pattern

--- passMeter.py
+++ passMeter.py
@@ -1,3 +1,5 @@
+from clint.textui import colored
+
 #key=[
 #        ['!','@','#','$','%','^','&','*','(',')','_','+'],
 #        ['Q','W','E','R','T','Y','U','I','O','P','{','}','|'],
@@ -22,7 +24,7 @@ def findPos(char):
                 return [row,col]
 
 def passMeter ():
-    print('-------------------------------------------')
+    print(colored.red('-------------------------------------------'))
     print('|                                         |')
     print('|           Welcome to *****              |')
     print('|                                         |')
@@ -31,7 +33,7 @@ def passMeter ():
     print('-------------------------------------------')
     print('')
    # while (True):
-    password = input('Enter a password: ')
+    password = input(colored.green('Enter a password: '))
     print('')
     #validation variables
     length= False
@@ -46,63 +48,81 @@ def passMeter ():
     #check length 
     if(len(password)>=8):
         length = True
-        print('The password has more than 8 digits')
+        print(colored.green('The password has more than 8 digits'))
     else:
-        print('X The password should have at least 8 digits')
+        print(colored.red('The password should have at least 8 digits'))
     
     #check capital letters
     for cap in password:
         if(cap.isupper()):
             capital=True 
-            print('The password has at least one capital letter')
+            print(colored.green('The password has at least one capital letter'))
             break
     if(not capital):
-        print('X The password shoud have at least one capital letter')
+        print(colored.red('The password shoud have at least one capital letter'))
     
     #check lowercase letters
     for low in password:
         if(low.islower()):
             lower=True 
-            print('The password has at least one lowercase letter')
+            print(colored.green('The password has at least one lowercase letter'))
             break
     if(not lower):
-        print('X The password shoud have at least one lowercase letter')
+        print(colored.red('The password shoud have at least one lowercase letter'))
     
     #check numbers
     for num in password:
         if(num.isdigit()):
             number=True 
-            print('The password has at least one number')
+            print(colored.green('The password has at least one number'))
             break
     if(not number):
-        print('X The password shoud have at least one number')
+        print(colored.red('The password shoud have at least one number'))
 
     #check symbols
     char= ['!','@','#','$','%','^','&','*','-','.',':','?','+','=','_']
     for sym in password:
         if(sym in char):
             symbol=True 
-            print('The password has at least one symbol')
+            print(colored.green('The password has at least one symbol'))
             break
     if(not symbol):
-        print('X The password shoud have at least one symbol')
+        print(colored.red('The password shoud have at least one symbol'))
     
     #check keyboard pattern 
     pos=[]
     for ch in password: 
         if(ch.islower() or ch.isupper() or ch.isdigit()):
             pos.append(findPos(ch.upper()))
-    print(pos)
-    row=0,col=0,i=0,rCount=0,cCount=0
+    #print(pos)
+    row=0 
+    col=0
+    i=0
+    rCount=0
+    cCount=0
     while row<4 and col<10 and i<len(pos):
         if pos[i][0]==row :
             rCount=rCount+1
         if pos[i][1]==col :
             cCount=cCount+1
+       # print("counter: "+str(rCount)+" row "+str(row)+" i "+str(i))
+       # print(i)
+        if cCount>2 or rCount>2:
+            pattern = True
+            print(colored.red('Stop using pattern stupid :)'))
+            break
         if pos[i][0] != row:
             rCount=0
-        if pos[i][1] != row:
-            rCount=0
+        if pos[i][1] != col:
+            cCount=0
+        if i+1 ==len(pos):
+            row = row +1 
+            col= col+1
+            i=0
+        i=i+1
+    if(not pattern):
+        print(colored.green('Wow :) '))
+
 
     #check is it a dictionary word or not
     openfile= open('/usr/share/dict/wordlist-probable.txt','r')
@@ -111,10 +131,11 @@ def passMeter ():
     for word in dictionary:
         if word.rstrip() == password:
             dic = True
-            print('The password is a dictionary word')
+            print(colored.red('The password is a dictionary word'))
             break
     if(not dic):
-        print('You are lucky. They did not discover your password :) ')
+        print(colored.green('You are lucky. They did not discover your password :) ')) 
 
 passMeter()
+
 
